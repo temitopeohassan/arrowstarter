9397f322c60b5ca802bd0d4368cbf4db110321d7
temitopeohassan <temitope.hassan.2@gmail.com>
2025-07-20 15:05:34 +0200

    edited API url in config file debugging cors issue

--- app/app/config.ts
+++ app/app/config.ts
@@ -1,4 +1,4 @@
 export const NEXT_PUBLIC_URL = 'https://arrowstarter.vercel.app';
-export const API_BASE_URL = 'https://arrowstarter-backend.vercel.app/';
-export const BACK_END_API = 'https://arrowstarter-backend.vercel.app/'; 
+export const API_BASE_URL = 'https://arrowstarter-backend.vercel.app';
+export const BACK_END_API = 'https://arrowstarter-backend.vercel.app'; 
 

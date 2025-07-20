cf30b2430ba692b4425dba3616ed8bf4d2ab1fa2
temitopeohassan <temitope.hassan.2@gmail.com>
2025-07-20 14:37:04 +0200

    edited API url in config file

--- app/app/config.ts
+++ app/app/config.ts
@@ -1,11 +1,4 @@
-const isLocalhost = typeof window !== 'undefined' && window.location.hostname === 'localhost';
+export const NEXT_PUBLIC_URL = 'https://arrowstarter.vercel.app';
+export const API_BASE_URL = 'https://arrowstarter-backend.vercel.app/';
+export const BACK_END_API = 'https://arrowstarter-backend.vercel.app/'; 
 
-export const NEXT_PUBLIC_URL = isLocalhost
-  ? 'http://localhost:3000'
-  : 'https://arrowstarter.vercel.app';
-
-export const API_BASE_URL = isLocalhost
-  ? 'http://localhost:8080'
-  : 'https://arrowstarter-backend.vercel.app';
-
-export const BACK_END_API = API_BASE_URL; // reuse the same base

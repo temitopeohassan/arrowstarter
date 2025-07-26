f15dfea1d416c893969f8f494f50954411a3e45c
temitopeohassan <temitope.hassan.2@gmail.com>
2025-07-26 04:22:40 +0100

    rainbow 3

--- app/app/components/wagmi-config-provider.tsx
+++ app/app/components/wagmi-config-provider.tsx
@@ -1,16 +1,21 @@
 'use client';
 
 import { ReactNode, useState } from 'react';
-import { WagmiProvider } from 'wagmi';
+import { WagmiProvider, State } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { config } from '../wagmi';
 
-export function WagmiConfigProvider({ children }: { children: ReactNode }) {
+type ProvidersProps = {
+  children: React.ReactNode;
+  initialState?: State; // 👈 Accept initialState as optional
+};
+
+export function Providers({ children, initialState }: ProvidersProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={config} initialState={initialState}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           {children}
